d2ff903ecebd379a1267f31f1899d68ee01410f7
Brooke Lewis <belv2c@gmail.com>
2020-01-03 19:28:37 -0600

    initial routing configuration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.scss';
-import { Button } from 'carbon-components-react';
 import { Content } from 'carbon-components-react/lib/components/UIShell';
 import Header from './components/Header';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 function App() {
   return (
     <>
       <Header />
         <Content>
-          <Button>Button</Button>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
         </Content>
     </>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import Notification20 from '@carbon/icons-react/lib/notification/20';
-import UserAvatar20 from '@carbon/icons-react/lib/user--avatar/20';
-import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
-
 import {
   Header,
   HeaderName,
@@ -12,28 +8,34 @@ import {
   HeaderGlobalAction,
   SkipToContent,
 } from 'carbon-components-react/lib/components/UIShell';
+import Notification20 from '@carbon/icons-react/lib/notification/20';
+import UserAvatar20 from '@carbon/icons-react/lib/user--avatar/20';
+import AppSwitcher20 from '@carbon/icons-react/lib/app-switcher/20';
+import { Link } from 'react-router-dom';
 
 const AppHeader = () => (
   <Header aria-label="Github Profile">
     <SkipToContent />
-    <HeaderName href="/" prefix="IBM">
-      Github API Profile
+    <HeaderName element={Link} to="/" prefix="Github">
+      Profile
     </HeaderName>
     <HeaderNavigation aria-label="Github Profile">
-      <HeaderMenuItem href="/repos">Repositories</HeaderMenuItem>
+      <HeaderMenuItem element={Link} to="/repos">
+        Repositories
+      </HeaderMenuItem>
     </HeaderNavigation>
     <HeaderGlobalBar>
-        <HeaderGlobalAction aria-label="Notifications">
-            <Notification20 />
-        </HeaderGlobalAction>
-        <HeaderGlobalAction aria-label="User Avatar">
-            <UserAvatar20 />
-        </HeaderGlobalAction>
-        <HeaderGlobalAction aria-label="App Switcher">
-            <AppSwitcher20 />
-        </HeaderGlobalAction>
+      <HeaderGlobalAction aria-label="Notifications">
+        <Notification20 />
+      </HeaderGlobalAction>
+      <HeaderGlobalAction aria-label="User Avatar">
+        <UserAvatar20 />
+      </HeaderGlobalAction>
+      <HeaderGlobalAction aria-label="App Switcher">
+        <AppSwitcher20 />
+      </HeaderGlobalAction>
     </HeaderGlobalBar>
   </Header>
 );
 
-export default Header;
+export default AppHeader;
