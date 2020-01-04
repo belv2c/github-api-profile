96ea55fc042c044a0f9d63669ffc7d5c56d4e7af
Brooke Lewis <belv2c@gmail.com>
2020-01-03 19:16:09 -0600

    initial ui shell configuration

--- src/content/LandingPage/index.js
+++ src/content/LandingPage/index.js
@@ -1,1 +1,4 @@
 import React from 'react';
+import LandingPage from './LandingPage';
+
+export default LandingPage;
--- src/content/RepoPage/index.js
+++ src/content/RepoPage/index.js
@@ -1,1 +1,3 @@
-import React from 'react';
+import RepoPage from './RepoPage';
+
+export default RepoPage;
